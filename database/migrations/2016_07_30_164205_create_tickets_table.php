e8d57cb6621eaedbac9a7742772e4679c45ff930
Ivan Rey De Cardo <ivanreydecardo@gmail.com>
2016-07-31 00:58:16 +0800

    Fixed constraints when seeding

--- database/migrations/2016_07_30_164205_create_tickets_table.php
+++ database/migrations/2016_07_30_164205_create_tickets_table.php
@@ -14,9 +14,9 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned;
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('movie_id')->unsigned;
+            $table->integer('movie_id')->unsigned();
             $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
             $table->timestamp('schedule');
             $table->string('cinema');
